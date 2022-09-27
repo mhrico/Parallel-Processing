2af7cd3f4275c98ad14fd90e02604191ef8c2446
Mehedi Hassan Rico <mhrico2@gmail.com>
2022-09-27 16:04:38 +0600

    Explanations of stuff in comments

--- 1_Ranks.c
+++ 1_Ranks.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 #include<mpi.h>
 
+// MPI likes working with references. So instead of directly accessing variables, put & infront of them
+
 int main(int argc, char** argv)
 {
-    MPI_Init(&argc, &argv);
+    MPI_Init(&argc, &argv); // Call this if you want to do MPI stuff
 
-    int myRank, worldSize;
+    int myRank, worldSize; // Gonna store process rank in myRank, and total processes in worldSize
 
-    MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+    MPI_Comm_size(MPI_COMM_WORLD, &worldSize); // This gets total processes and stores it in worldSize. Here COMM_WORLD is the default communicator
+    MPI_Comm_rank(MPI_COMM_WORLD, &myRank); // Gets rank and puts it in myRank. You want to use both of this functions in your programs.
 
     printf("Process rank: %d; Total processes: %d\n", myRank, worldSize);
 
-    MPI_Finalize();
+    MPI_Finalize(); // Call this when you are doing doing MPI stuff or bad things will happen to ur OS
 }
